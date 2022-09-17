5018470f0e289837ce0af5c86b4b4e970f82ac27
Java-Sith <prieto.luisdaniel@gmail.com>
2022-09-17 03:23:30 -0600

    Se empieza a reescribir la lectura y escritura

--- Proyecto1-Arqui2/main.py
+++ Proyecto1-Arqui2/main.py
@@ -13,6 +13,9 @@ P3 = [["B1", "I", "0x101", 0], ["B2", "I", "0x011", 0], ["B3", "I", "0x111", 0],
 P4 = [["B1", "I", "0x011", 0], ["B2", "I", "0x111", 0], ["B3", "I", "0x011", 0], ["B4", "I", "0x101", 0]]
 Memory = {"0x000": 0, "0x001": 0, "0x010": 0, "0x011": 0, "0x100": 0, "0x101": 0, "0x110": 0, "0x111": 0}
 Instructions = {0: "P0: READ 0100", 1: "P1: CALC", 2: "P2: WRITE 1010, 4A3B", 3: "P3: CALC"}
+readMiss = 0
+writeMiss = 0
+Processors = {P1, P2, P3, P4}
 
 # def five_seconds():
 #     time.sleep(5)
@@ -71,25 +74,10 @@ def state_change(current_state, action, processor, row):
             processor[row][1] = "S"
 
 def read_inst(mem_block, data, processor):
-    if(mem_block is not None):
-        if(mem_block == "0x000" or mem_block == "0x100"):
-            processor[0][1] = "M"
-            processor[0][3] = data
-            processor[0][2] = "0x000"
-        elif(mem_block == "0x001" or mem_block == "0x101"):
-            processor[1][1] = "M"
-            processor[1][3] = data
-            processor[1][2] = "0x001"
-        elif(mem_block == "0x010" or mem_block == "0x110"):
-            processor[2][1] = "M"
-            processor[2][3] = data
-            processor[2][2] = "0x010"
-        elif(mem_block == "0x011" or mem_block == "0x111"):
-            processor[3][1] = "M"
-            processor[3][3] = data
-            processor[3][2] = "0x011"
-    else:
-        print("This didn't work")
+    for i in range(4):
+        for j in range(4):
+            if (P1[i][j] == mem_block):
+                processor[i][j] = data
 
 def write_back(mem_block, data, processor):
     if(mem_block is not None):
