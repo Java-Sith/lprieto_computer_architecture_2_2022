40fff410ec2191d1178f63943cd774dbf570cee5
Java-Sith <prieto.luisdaniel@gmail.com>
2022-09-21 04:35:08 -0600

    Se agrega la documentación interna

--- Proyecto1-Arqui2/main.py
+++ Proyecto1-Arqui2/main.py
@@ -5,21 +5,30 @@ from random import randint
 from threading import *
 from random import *
 
+# Se define la ventana dándole una geometría determinada y título
 root = Tk()
 root.title("MESI Cache Simulator!")
 root.geometry("900x600")
 
+# Se definen las variables globales como matrices para cada uno de los procesadores
+# Cada una de las filas representa un bloque de caché 
+# Cada una de las columnas están organizadas de la forma: Bloque, Estado, Dirección de Memoria y Valor
 P0 = [["B1", "I", "0000", "0000"], ["B2", "I", "0000", "0000"], ["B3", "I", "0000", "0000"], ["B4", "I", "0000", "0000"]]
 P1 = [["B1", "I", "0000", "0000"], ["B2", "I", "0000", "0000"], ["B3", "I", "0000", "0000"], ["B4", "I", "0000", "0000"]]
 P2 = [["B1", "I", "0000", "0000"], ["B2", "I", "0000", "0000"], ["B3", "I", "0000", "0000"], ["B4", "I", "0000", "0000"]]
 P3 = [["B1", "I", "0000", "0000"], ["B2", "I", "0000", "0000"], ["B3", "I", "0000", "0000"], ["B4", "I", "0000", "0000"]]
+# Se define la variable global como diccionario de la memoria principal
+# Este está definido de la forma Llave : Valor como Dirección de memoria : Dato
 Memory = {"0000": "0000", "0001": "0000", "0010": "0000", "0011": "0000", "0100": "0000", "0101": "0000", "0110": "0000", "0111": "0000"}
+
+#Se definen otras variables globales a utilizar más adelante en el programa
 readMiss = 0
 writeMiss = 0
 readHit = 0
 writeHit = 0
 Instructions = []
 
+#Mediante la librería Tkinter se definen los Treeview que simulan cada procesador
 my_tree1 = ttk.Treeview(root)
 my_tree2 = ttk.Treeview(root)
 my_tree3 = ttk.Treeview(root)
@@ -28,13 +37,19 @@ my_tree5 = ttk.Treeview(root)
 my_tree6 = ttk.Treeview(root)
 my_tree7 = ttk.Treeview(root)
 
+#Los primeros 4 Treeview representan cada uno de los procesadores
 my_tree1['columns'] = ("Estado", "Direccion", "Dato")
 my_tree2['columns'] = ("Estado", "Direccion", "Dato")
 my_tree3['columns'] = ("Estado", "Direccion", "Dato")
 my_tree4['columns'] = ("Estado", "Direccion", "Dato")
+#Treeview de la Memoria
 my_tree5['columns'] = ("Dato")
+#Treeview de las instrucciones de los procesadores
 my_tree6['columns'] = ("Instruccion")
+#Treeview de los misses de lectura y escritura
 my_tree7['columns'] = ("ReadMiss", "WriteMiss")
+
+### A cada uno los Treeview se les define las columnas respectivas, dirección de anclaje y longitud ###
 
 my_tree1.column("#0", width=100, minwidth=20)
 my_tree1.column("Estado", anchor = CENTER, width=60)
@@ -66,6 +81,8 @@ my_tree7.column("#0", width=20, minwidth=0)
 my_tree7.column("ReadMiss", anchor = CENTER, width=100)
 my_tree7.column("WriteMiss", anchor = CENTER, width=100)
 
+### A cada uno los Treeview se les define los encabezados respectivas, dirección de anclaje y texto ###
+
 my_tree1.heading("#0", text = "P0", anchor = CENTER)
 my_tree1.heading("Estado", text = "Estado", anchor = CENTER)
 my_tree1.heading("Direccion", text = "Direccion", anchor = CENTER)
@@ -96,26 +113,31 @@ my_tree7.heading("#0", text = "()", anchor = CENTER)
 my_tree7.heading("ReadMiss", text = "ReadMiss", anchor = CENTER)
 my_tree7.heading("WriteMiss", text = "WriteMiss", anchor = CENTER)
 
+# Se insertan los datos del primer procesador en el Treeview, tomando el bloque como texto y los valores como una tupla
 my_tree1.insert(parent='', index='end', text=P0[0][0], values=(P0[0][1], P0[0][2], P0[0][3]))
 my_tree1.insert(parent='', index='end', text=P0[1][0], values=(P0[1][1], P0[1][2], P0[1][3]))
 my_tree1.insert(parent='', index='end', text=P0[2][0], values=(P0[2][1], P0[2][2], P0[2][3]))
 my_tree1.insert(parent='', index='end', text=P0[3][0], values=(P0[3][1], P0[3][2], P0[3][3]))
 
+# Se insertan los datos del segundo procesador en el Treeview, tomando el bloque como texto y los valores como una tupla
 my_tree2.insert(parent='', index='end', text=P1[0][0], values=(P1[0][1], P1[0][2], P1[0][3]))
 my_tree2.insert(parent='', index='end', text=P1[1][0], values=(P1[1][1], P1[1][2], P1[1][3]))
 my_tree2.insert(parent='', index='end', text=P1[2][0], values=(P1[2][1], P1[2][2], P1[2][3]))
 my_tree2.insert(parent='', index='end', text=P1[3][0], values=(P1[3][1], P1[3][2], P1[3][3]))
 
+# Se insertan los datos del tercer procesador en el Treeview, tomando el bloque como texto y los valores como una tupla
 my_tree3.insert(parent='', index='end', text=P2[0][0], values=(P2[0][1], P0[0][2], P0[0][3]))
 my_tree3.insert(parent='', index='end', text=P2[1][0], values=(P2[1][1], P0[1][2], P0[1][3]))
 my_tree3.insert(parent='', index='end', text=P2[2][0], values=(P2[2][1], P0[2][2], P0[2][3]))
 my_tree3.insert(parent='', index='end', text=P2[3][0], values=(P2[3][1], P0[3][2], P0[3][3]))
 
+# Se insertan los datos del cuarto procesador en el Treeview, tomando el bloque como texto y los valores como una tupla
 my_tree4.insert(parent='', index='end', text=P3[0][0], values=(P0[0][1], P0[0][2], P0[0][3]))
 my_tree4.insert(parent='', index='end', text=P3[1][0], values=(P0[1][1], P0[1][2], P0[1][3]))
 my_tree4.insert(parent='', index='end', text=P3[2][0], values=(P0[2][1], P0[2][2], P0[2][3]))
 my_tree4.insert(parent='', index='end', text=P3[3][0], values=(P0[3][1], P0[3][2], P0[3][3]))
 
+# Se insertan los datos de la memoria principal en el Treeview, tomando la dirección de memoria como texto y los valores de cada llave
 my_tree5.insert(parent='', index='end', text="0000", values=Memory["0000"])
 my_tree5.insert(parent='', index='end', text="0001", values=Memory["0001"])
 my_tree5.insert(parent='', index='end', text="0010", values=Memory["0010"])
@@ -125,11 +147,13 @@ my_tree5.insert(parent='', index='end', text="0101", values=Memory["0101"])
 my_tree5.insert(parent='', index='end', text="0110", values=Memory["0110"])
 my_tree5.insert(parent='', index='end', text="0111", values=Memory["0111"])
 
+# Se insertan los datos de las instrucciones de cada procesador en el Treeview, tomando el procesador como texto y los valores de cada instrucción
 my_tree6.insert(parent='', index='end', text="P0", values="None")
 my_tree6.insert(parent='', index='end', text="P1", values="None")
 my_tree6.insert(parent='', index='end', text="P2", values="None")
 my_tree6.insert(parent='', index='end', text="P3", values="None")
 
+# Se colocan los Treeview de cada uno de los datos requeridos en la interfaz gráfica
 my_tree1.place(x=10, y=10)
 my_tree2.place(x=410, y=10)
 my_tree3.place(x=10, y=310)
@@ -138,41 +162,48 @@ my_tree5.place(x=810, y=10)
 my_tree6.place(x=810, y=510)
 my_tree7.place(x=810, y=260)
 
-# my_label = Label(root, text="I made a thread")
-# my_label.pack(pady=20)
-
-# random_label = Label(root, text="Random number")
-# random_label.pack(pady=20)
-
-# def rando():
-#     random_label.config(text=f'Random number is: {randint(1, 100)}')
-
 
 def fact(x):
+    #Factorial básico necesario para calcular la distribución binomial
     f = 1
     for i in range(x):
         f = f * (i + 1)
     return f
 
 def bincoeff(a, b):
+    # Se obtiene el factor multiplicativo de los factoriales, dada la C
     comb = fact(a) / (fact(b) * fact(a - b))
     return comb
 
 def binomial_dist(n, k, p):
+    # Se obtiene la función de distribución binomial mediante la fórmula
+    # n es la cantidad de experimentos
+    # p es la probabilidad de éxito
+    # k es la cantidad de éxitos
     dist = bincoeff(n, k) * (p ** k) * ((1 - p) ** (n - k))
+    # Para mejor uso de los datos, se redondea el resultado y se multiplica por 10
     return round(dist * 10)
 
 def write_back(mem_block, data):
+    # Busca el bloque de memoria correspondiente
     if(mem_block is not None):
+        # Ciclo for para recorrer el bloque
         for key in Memory:
+            # Actualiza el valor en memoria con la llave
             if(mem_block == key):
                 Memory[key] = data
     else:
         print("This didn't work")
 
+# Recibe el estado actual, la acción realizada, el procesador que la realiza, el bloque
+# Recibe un booleano que determina si está en memoria
+# Read y Write es lectura y escritura, ReadCache y WriteCache es veo lectura o escritura
 def state_change(current_state, action, processor, row, inMemory):
+    # Mediante la máquina de estados vista en clase, cambia al estado correspondiente del estado actual
+    # Si el caché está inválido
     if(current_state == "I"):
         if(action == "Read"):
+            #Verifica si el dato proviene de memoria para pasar a E ó S dado el caso
             if(inMemory):
                 processor[row][1] = "E"
             else:
@@ -180,18 +211,23 @@ def state_change(current_state, action, processor, row, inMemory):
         elif(action == "Write"):
             print("Debería cambiar de estado!")
             processor[row][1] = "M"
+    # Si el caché está compartido
     elif(current_state == "S"):
         if(action == "WriteCache"):
             processor[row][1] = "I"
         elif(action == "Write"):
             processor[row][1] = "M"
+    # Si el caché está modificado
     elif(current_state == "M"):
         if(action == "WriteCache"):
             processor[row][1] = "I"
+            # Realiza el write-back correspondiente a memoria
             write_back(processor[row][2], processor[row][3])
         elif(action == "ReadCache"):
             processor[row][1] = "S"
+            # Realiza el write-back correspondiente a memoria
             write_back(processor[row][2], processor[row][3])
+    # Si el caché está exclusivo
     elif(current_state == "E"):
         if(action == "WriteCache"):
             processor[row][1] = "I"
@@ -200,9 +236,14 @@ def state_change(current_state, action, processor, row, inMemory):
         elif(action == "ReadCache"):
             processor[row][1] = "S"
 
-def read_memory(mem_block, processor, row):
+# Si no encontró el dato en lac cachés, lo va a buscar a memoria
+# Recibe el bloque de memoria y el procesador respectivo
+def read_memory(mem_block, processor):
     for key in Memory:
+        # Busca la llave en el For
         if (key == mem_block):
+            # Por asociatvidad one-way, asigna la variable correspondiente
+            # Según la dirección de memoria
             if(key == "0000" or key == "0100"):
                 processor[0][2] = key
                 processor[0][3] = Memory[key]
@@ -224,14 +265,19 @@ def read_memory(mem_block, processor, row):
                 break
 
 def write_inst(mem_block, processor, data):
+    # Usa las variables globales de aciertos y desaciertos
     global writeMiss
     global writeHit
+    # Busca la dirección de memoria en caché del procesador dado
     for i in range(4):
         if(processor[i][2] == mem_block):
             processor[i][3] = data
             writeHit += 1
             return i, False
+    # De lo contrario asigna el valor
     else:
+        # Por asociatvidad one-way, escribe la variable correspondiente
+        # Según la dirección de memoria
         if(mem_block == "0000" or mem_block == "0100"):
             processor[0][2] = mem_block
             processor[0][3] = data
@@ -254,49 +300,65 @@ def write_inst(mem_block, processor, data):
             return 3, False   
 
 def read_inst(mem_block, processor):
+    # Usa las variables globales de aciertos y desaciertos
     global readMiss
     global readHit
+    # Busca la dirección de memoria en caché del procesador dado
     for i in range(4):
         if(processor[i][2] == mem_block and processor[i][1] != "I"):
             readHit += 1
             return i, False
+    # De lo contrario consulta a las demás cachés
     else:
+        # Busca la dirección de memoria en caché L1 del P0
+        # Si la encuentra actualiza los valores correspondientes del procesador
         for i in range(4):
             if (P0[i][2] == mem_block and P0[i][1] == "E"):
                 processor[i][3] = P0[i][3]
                 processor[i][2] = P0[i][2]
                 readMiss += 1
                 return i, False
+        # Busca la dirección de memoria en caché L1 del P0
+        # Si la encuentra actualiza los valores correspondientes del procesador
         for i in range(4):
             if (P1[i][2] == mem_block and P1[i][1] == "E"):
                 processor[i][3] = P1[i][3]
                 processor[i][2] = P1[i][2]
                 readMiss += 1
                 return i, False
+        # Busca la dirección de memoria en caché L1 del P0
+        # Si la encuentra actualiza los valores correspondientes del procesador
         for i in range(4):
             if (P2[i][2] == mem_block and P2[i][1] == "E"):
                 processor[i][3] = P2[i][3]
                 processor[i][2] = P2[i][2]
                 readMiss += 1
                 return i, False
+        # Busca la dirección de memoria en caché L1 del P0
+        # Si la encuentra actualiza los valores correspondientes del procesador
         for i in range(4):    
             if (P3[i][2] == mem_block and P3[i][1] == "E"):
                 processor[i][3] = P3[i][3]
                 processor[i][2] = P3[i][2]
                 readMiss += 1
                 return i, False
+        # Si no encuentra la dirección de memoria en caché, va a leer de memoria
         else:
             readMiss += 1
             row, inMemory = read_memory(mem_block, processor, i)
             return row, inMemory
 
 def generate_inst(proce, lock):
+    # Aplica un lock para que otro hilo no pida una instrucción
     lock.acquire()
+    # Parámetros de la distribución binomial
     n = randint(0, 10)
     k = randint(0, 5)
     p = randint(1, 5) / randint(5, 10)
+    # Se ejecuta la distribución binomial
     dist = binomial_dist(n, k, p)
     processor = proce
+    # Asigna una instrucción al procesador P0 dependiendo del resultado de la distribución binomial
     if(processor == P0):
         if(dist == 0):
             inst = "P0 " + "READ" + " " + format(randint(0, 7), "04b")
@@ -304,6 +366,7 @@ def generate_inst(proce, lock):
             inst = "P0 " + "WRITE" + " " + format(randint(0, 7), "04b") + " " + format(randint(0, 65535), "04X")
         else:
             inst = "P0 " + "CALC"
+    # Asigna una instrucción al procesador P1 dependiendo del resultado de la distribución binomial
     elif(processor == P1):
         if(dist == 0):
             inst = "P1 " + "READ" + " " + format(randint(0, 7), "04b")
@@ -311,6 +374,7 @@ def generate_inst(proce, lock):
             inst = "P1 " + "WRITE" + " " + format(randint(0, 7), "04b") + " " + format(randint(0, 65535), "04X")
         else:
             inst = "P1 " + "CALC"
+    # Asigna una instrucción al procesador P2 dependiendo del resultado de la distribución binomial
     elif(processor == P2):
         if(dist == 0):
             inst = "P2 " + "READ" + " " + format(randint(0, 7), "04b")
@@ -318,6 +382,7 @@ def generate_inst(proce, lock):
             inst = "P2 " + "WRITE" + " " + format(randint(0, 7), "04b") + " " + format(randint(0, 65535), "04X")
         else:
             inst = "P2 "+ "CALC"
+    # Asigna una instrucción al procesador P3 dependiendo del resultado de la distribución binomial
     elif(processor == P3):
         if(dist == 0):
             inst = "P3 " + "READ" + " " + format(randint(0, 7), "04b")
@@ -325,8 +390,11 @@ def generate_inst(proce, lock):
             inst = "P3 " + "WRITE" + " " + format(randint(0, 7), "04b") + " " + format(randint(0, 65535), "04X")
         else:
             inst = "P3 " + "CALC"
+    # Interrumpe por una décima de unidad de tiempo para sincronizar los hilos
     sleep(0.1)
+    # Agrega las instrucciones a la lista
     Instructions.append(inst)
+    # Libera el candado para su uso
     lock.release()
     return inst
 
